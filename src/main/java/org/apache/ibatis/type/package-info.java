439475d5b13b47270ca8f030c8e442e28365703e
wangzuojun <liveforothers@aliyun.com>
2020-08-07 20:31:42 +0800

    type module

--- src/main/java/org/apache/ibatis/type/package-info.java
+++ src/main/java/org/apache/ibatis/type/package-info.java
@@ -15,5 +15,10 @@
  */
 /**
  * Type handlers.
+ * 类型转换模块
+ * 1 为了简化配置文件提供了别名机制，该机制是类型转换模块的主要功能之一。
+ * 2 实现 JDBC 类型与 Java 类型之间的转换，该功能在
+ *   2.1 为SQL语句绑定实参：为 SQL 语句绑定实参时，会将数据由 Java 类型转换成 JDBC 类型。
+ *   2.2 映射查询结果集：将数据由 JDBC 类型转换成 Java 类型
  */
 package org.apache.ibatis.type;
